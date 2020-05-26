1450a44c9a8814b62014ef2ef77179cebee4d125
rohit agarwal <rohitagarwal516@gmail.com>
2020-05-26 21:55:39 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,15 @@
+			public class Max
+{
+ public static void main(String args[])
+{
+  int a,b;
+  a=5;
+  b=10;
+  if(a>b)
+    {
+      system.out.println(a is greater); 
+     }
+  else
+      system.out.println(b is greater);
+  }
+}
