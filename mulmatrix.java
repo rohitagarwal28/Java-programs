904e72037f3ed66bb46755e3f3bb15df5c2d088e
rohit agarwal <rohitagarwal516@gmail.com>
2020-05-28 23:10:32 +0530

    Add files via upload

---
+++ mulmatrix.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class mulmatrix
+{
+ public static void main(String args[])
+ {
+  int a[][]=new int[3][3];
+  int b[][]=new int[3][3];
+  int c[][]=new int[3][3];
+  int i,j,k;
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter first array");
+   for(i=0;i<3;i++)
+    {
+     for(j=0;j<3;j++)
+	{
+          a[i][j]=sc.nextInt();
+ 	}
+    }
+for(i=0;i<3;i++)
+    {
+     for(j=0;j<3;j++)
+	{
+          System.out.print("\t"+a[i][j]);
+        }
+       System.out.println();
+    }	
+System.out.println("enter second array");
+   for(i=0;i<3;i++)
+    {
+     for(j=0;j<3;j++)
+	{
+          b[i][j]=sc.nextInt();
+ 	}
+    }
+for(i=0;i<3;i++)
+    {
+     for(j=0;j<3;j++)
+	{
+          System.out.print("\t"+b[i][j]);
+        }
+       System.out.println();
+    }	
+    System.out.println("matrix multiplication=");
+   for(i=0;i<3;i++)
+    {
+     for(j=0;j<3;j++)
+	{
+	  for(k=0;k<3;k++)
+           {
+ 	     
+         c[i][j]=c[i][j]+a[i][k]*b[k][j];
+ 	}
+    }
+}
+
+     for(i=0;i<3;i++)
+    {
+     for(j=0;j<3;j++)
+	{
+    System.out.print("\t"+c[i][j]);
+}
+System.out.println();
+}
+}
+}
