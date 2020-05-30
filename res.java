42cda3635c4e7d836981270adf29c7ea3b532231
rohit agarwal <rohitagarwal516@gmail.com>
2020-05-30 22:34:50 +0530

    Add files via upload

---
+++ res.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+class Rectanglee
+{
+ public
+ int l,b,area;
+ Scanner sc=new Scanner(System.in);
+ void input()
+{
+ System.out.println("enter the length and breadth of rectangle"); 
+ l=sc.nextInt();
+ b=sc.nextInt();
+ area=l*b;
+}
+ void show()
+{
+ System.out.println(area);
+}
+}
+class Cuboid extends Rectanglee
+{
+ public
+ int h,volume;
+ void read()
+{
+ System.out.println("enter the height of cuboid");
+ h=sc.nextInt();
+ volume=super.l*super.b*h;
+}
+void display()
+{
+ System.out.println("volume="+volume);
+}
+}
+class res
+{
+ public static void main(String args[])
+ {
+   Cuboid r=new Cuboid();
+   r.input();
+   r.show();
+   r.read();
+   r.display();
+}
+}
+ 
+ 
